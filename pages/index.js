027d34e74967667a9323c4b1fd3407d5ec697afa
PizzaPrix <seanwaterloo2997@gmail.com>
2023-02-11 15:35:50 -0500

    Create level direction system

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
-import React from "react";
+import { useState } from "react";
 import Level1 from "./level1";
+import Level2 from "./Level2";
 
 export default function Home() {
+    const [level, setLevel] = useState(1);
+
+    let currentLevel
+
+    if (level === 1) {
+        currentLevel = <Level1 />
+    }
+    else if (level === 2) {
+        currentLevel = <Level2 />
+    }
+
     return (
         <div>
-            <Level1 />
+            { currentLevel }
         </div>
     )
 }
